e2cd83168d41a8069fa565213983d1a1ef7f191c
AndrewJBateman <gomezbateman@yahoo.com>
2021-06-24 17:45:43 +0200

    feat: graphql & prisma config. files added

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,5 +1,51 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { ApolloServer, gql } from 'apollo-server-micro';
+import { PrismaClient } from '@prisma/client';
 
-export default function helloAPI(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const prisma = new PrismaClient();
+
+// define model: what queries/mutations should do
+const typeDefs = gql`
+  type Card {
+    id: Int!
+    name: String!
+    email: String!
+    phone: String!
+    biography: String!
+    cardId: String!
+    twitter: String!
+    github: String!
+    website: String!
+  }
+  type Query {
+    getCards: [Card]
+
+  }
+`;
+
+// used to carry out queries/mutations
+const resolvers = {
+  Query: {
+    getCards: async () => {
+      return await prisma.card.findMany({
+        take: 10
+      });
+    }
+  }
+};
+
+export const config = {
+  api: {
+    bodyParser: false
+  }
 }
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers
+})
+
+const handler = apolloServer.createHandler({
+  path: '/api/graphql'
+})
+
+export default handler;
